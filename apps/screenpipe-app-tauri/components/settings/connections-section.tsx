3e434ca6c171f1a9033ef4c43712e22eb0f49259
Anshgrover23 <anshgrover938@gmail.com>
2026-07-22 13:58:59 +0530

    fix(connections): official 2026 gradient icons for gmail, drive, docs, sheets

--- apps/screenpipe-app-tauri/components/settings/connections-section.tsx
+++ apps/screenpipe-app-tauri/components/settings/connections-section.tsx
@@ -511,15 +511,7 @@ const INTEGRATION_ICONS: Record<string, React.ReactNode> = {
         />
       </svg>
     ),
-    gmail: (
-      <svg viewBox="0 0 256 193" className="w-5 h-5" preserveAspectRatio="xMidYMid meet" aria-hidden>
-        <path fill="#4285F4" d="M58.182 192.05V93.14L27.507 65.077 0 49.504v125.091c0 9.658 7.825 17.455 17.455 17.455h40.727Z"/>
-        <path fill="#34A853" d="M197.818 192.05h40.727c9.659 0 17.455-7.826 17.455-17.455V49.505l-31.156 17.837-27.026 25.798v98.91Z"/>
-        <path fill="#EA4335" d="m58.182 93.14-4.174-38.647 4.174-36.989L128 69.868l69.818-52.364 4.669 34.992-4.669 40.644L128 145.504z"/>
-        <path fill="#FBBC04" d="M197.818 17.504V93.14L256 49.504V26.231c0-21.585-24.64-33.89-41.89-20.945l-16.292 12.218Z"/>
-        <path fill="#C5221F" d="m0 49.504 26.759 20.07L58.182 93.14V17.504L41.89 5.286C24.61-7.66 0 4.646 0 26.231v23.273Z"/>
-      </svg>
-    ),
+    gmail: <img src="/images/gmail.svg" alt="Gmail" className="w-5 h-5" />,
     "google-calendar": <img src="/images/google-calendar.svg" alt="Google Calendar" className="w-5 h-5" />,
     "google-docs": <img src="/images/google-docs.svg" alt="Google Docs" className="w-5 h-5" />,
     "google-drive": <img src="/images/google-drive.svg" alt="Google Drive" className="w-5 h-5" />,
